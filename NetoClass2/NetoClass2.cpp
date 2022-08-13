2f23ac566a78251647bf4d09064cb60a9301884a
DenisMir <pete4kob12@mail.ru>
2022-08-13 11:23:54 +0300

    workAfterIgnore

---
+++ NetoClass2/NetoClass2.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+
+class Counter
+{
+public:
+    int count;
+    int comm1;
+    
+public:
+    Counter()
+    {
+        this->count = 1;
+        this->comm1 = 0;
+    }
+    int add()
+    {
+        return count++;
+    }
+    int less()
+    {
+        return count--;
+    }
+    int counter()
+    {
+        return count;
+    }
+};
+void answUser(Counter &p)
+{
+    char answ; //я толи забыл то ли неумею через строку спрашивать пользователя ответ, 
+    //если std::string answ то у меня неполучается сравнивать answ == "Да" 
+    //проскакивает код дальше без проверок
+    //и еще не получается разобраться как .gitignore теперь прикрепить=)
+
+    std::cout << "Вы хотите указать начальное значение счётчика? Введите Y или N: \n";
+    std::cin >> answ;
+
+    if (answ == 'Y' || answ == 'y')
+    {
+        std::cout << "Введите начальное значение счетчика: ";
+        std::cin >> p.count;
+    }
+    if (answ == 'N' || answ == 'n')
+    {
+        std::cout << "Тогда счетчик установлен с 1 \n";
+    }
+
+}
+int counterUser(Counter &p)
+{
+    int count;
+    char command;
+    int comm1;
+    while (true)
+    {
+        std::cout << "Введите команду ('+', '-', '=' или 'x'): ";
+        std::cin >> command;
+        if (command == '+')
+        {
+            p.add();
+            std::cout << p.counter();
+            std::cout << std::endl;
+        }
+        if (command == '-')
+        {
+            p.less();
+            std::cout << p.counter();
+            std::cout << std::endl;
+        }
+        if (command == '=')
+        {
+            std::cout << p.counter();
+            std::cout << std::endl;
+        }
+        if (command == 'x')
+        {
+            std::cout << "Гудбай май лав гудбай)!\n";
+            return false;
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+
+    Counter counteR;
+    answUser(counteR);
+    counterUser(counteR);
+}
